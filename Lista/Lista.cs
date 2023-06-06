41570a3be08238d94be8eb619eec1473df870b81
RafaPerez05 <126112322+RafaPerez05@users.noreply.github.com>
2023-06-05 22:13:27 -0300

    exercicio 2 completo

--- Lista/Lista.cs
+++ Lista/Lista.cs
@@ -1,85 +1,193 @@
-public class Lista{
-
-    public No inicio;
-    public No fim;
-
-    public Lista(){
-        this.inicio = null;
-        this.fim = null;
-    }
-
-    public void inserirInicio(int Valor){
-        //INSERIR UM ELEMENTO NO INICIO DA LISTA
-        No novoNo = new(Valor);
-
-        //INSERINDO EM UMA LISTA VAZIA
-        if(this.inicio == null && this.fim == null){
-            this.inicio = novoNo;
-            this.fim = novoNo;
-        }
-        else if(this.inicio != null){ //LISTA NÃO VAZIA
-            novoNo.noProx = this.inicio;
-            this.inicio = novoNo;
-        }
-    }
-
-public void inserirFim(int Valor)//inserir no fim da lista
-    {
-        No novoNO = new(Valor);
-
-        if(this.inicio == null && this.fim == null){
-            this.inicio = novoNO;
-            this.fim = novoNO;
-        }
-
-        else if (this.fim.noProx == null){
-            this.fim.noProx = novoNO;
-            this.fim = novoNO;  
-        }
-
-
-    }
-    public void removerInicio(){//REMOVER ELEMENTO NO INICIO DA LISTA
-        if(this.inicio != null){
-            this.inicio = this.inicio.noProx;
-        }
-        else{
-            Console.Write("lista vazia");
-        }
-    }
-    public void removerFim(){
-    No noAtual = this.inicio;
-    while (noAtual.noProx.noProx != null){
-        noAtual = noAtual.noProx;
-    }
-    noAtual.noProx = null;
+//estrutura do MENU
+public class Funcionario
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Telefone { get; set; }
+    public double Salario { get; set; }
 }
 
-    public Boolean consulta(int Valor, ref No noAtual, ref No noAnterior){ 
-        
-        noAtual = this.inicio;
-        noAnterior = null;
+public class No
+{
+    public Funcionario Dado { get; set; }
+    public No Proximo { get; set; }
+}
 
-        while(noAtual != null){
+public class Lista
+{
+    public No primeiro;
+    public No ultimo;
 
-            if(noAtual.valor == Valor){ //SE ENCONTROU O VALOR
-                return(true);
+    public void AdicionarInicio(Funcionario funcionario)
+{
+    No novoNo = new No()
+    {
+        Dado = funcionario,
+        Proximo = primeiro
+    };
+
+    primeiro = novoNo;
+
+    if (ultimo == null)
+        {
+        ultimo = primeiro;
+        }
+    }
+    public void AdicionarFim(Funcionario funcionario)
+    {
+        No novoNo = new No()
+        {
+            Dado = funcionario,
+            Proximo = null
+        };
+
+        if (primeiro == null)
+        {
+            primeiro = novoNo;
+            ultimo = novoNo;
+        }
+        else
+        {
+            ultimo.Proximo = novoNo;
+            ultimo = novoNo;
+        }
+    }
+    
+
+    public void Exibir()
+    {
+        No atual = primeiro;
+
+        while (atual != null)
+        {
+            Console.WriteLine($"Nome: {atual.Dado.Nome}, Idade: {atual.Dado.Idade}, Telefone: {atual.Dado.Telefone}, Salário: {atual.Dado.Salario}");
+            atual = atual.Proximo;
+        }
+    }
+
+        public int escolhido;
+        public string nome;
+        public int idade;
+        public string telefone;
+        public double salario;
+        public void Escolha(){
+
+        //adcionar no inicio
+        if(escolhido==1){
+        Console.Write("Digite o nome: ");
+        nome = Console.ReadLine();
+
+        Console.Write("Digite a idade: ");
+        idade = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite o telefone: ");
+        telefone = Console.ReadLine();
+
+        Console.Write("Digite o salário: ");
+        salario = Convert.ToDouble(Console.ReadLine());
+
+        AdicionarInicio(new Funcionario() { Nome = nome, Idade = idade, Telefone = telefone, Salario = salario });
+        Console.WriteLine("Funcionário adicionado com sucesso ao inicio da lista!");
+
+         }
+         //adcionar no fim
+            else if(escolhido==2){
+            Console.Write("Digite o nome:\n");
+            nome = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Digite a idade:\n");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o Telefone:\n");
+            telefone = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Digite o Salario:\n");
+            salario = Convert.ToDouble(Console.ReadLine());
+
+            AdicionarFim(new Funcionario() { Nome = nome, Idade = idade, Telefone = telefone, Salario = salario });
+            Console.WriteLine("Funcionário adicionado com sucesso ao final da lista!");
+
+        }
+            //buscar
+            else if(escolhido==3){
+            Console.Write("Digite o nome a ser buscado: ");
+            string nomeBusca = Console.ReadLine();
+
+            No atual = primeiro;
+            bool encontrado = false;
+
+            while (atual != null)
+            {
+             if (atual.Dado.Nome.ToLower() == nomeBusca.ToLower())
+            {
+            Console.WriteLine($"Funcionário encontrado - Nome: {atual.Dado.Nome}, Idade: {atual.Dado.Idade}, Telefone: {atual.Dado.Telefone}, Salário: {atual.Dado.Salario}");
+            encontrado = true;
             }
-            noAnterior = noAtual;
-            noAtual = noAtual.noProx;
-        }
-    
-        return(false);
-    }
 
-    public void percurso(){
-        
-        No noAux = this.inicio;
+            atual = atual.Proximo;
+            }
 
-        while(noAux != null){
-            Console.Write(noAux.valor + " -> ");
-            noAux = noAux.noProx;            
-        }
+    if (!encontrado)
+    {
+        Console.WriteLine("Funcionário não encontrado.");
     }
+            
+         }
+            //remover no inicio
+            else if(escolhido==4){
+
+            if (primeiro == null){
+            Console.WriteLine("A lista está vazia. Não há funcionários para remover.");
+            }
+
+             else
+            {
+                Funcionario funcionarioRemovido = primeiro.Dado;
+                primeiro = primeiro.Proximo;
+
+            if (primeiro == null){
+                 ultimo = null;
+            }
+            Console.WriteLine($"Funcionário removido do início da lista - Nome: {funcionarioRemovido.Nome}, Idade: {funcionarioRemovido.Idade}, Telefone: {funcionarioRemovido.Telefone}, Salário: {funcionarioRemovido.Salario}");
+            }
+
+         }
+            //remover no fim
+            else if(escolhido==5){
+
+            if (ultimo == null){
+                Console.WriteLine("A lista está vazia. Não há funcionários para remover.");
+            }
+            else{
+            if (primeiro == ultimo){
+            Funcionario funcionarioRemovido = primeiro.Dado;
+            primeiro = null;
+            ultimo = null;
+            Console.WriteLine($"Funcionário removido do fim da lista - Nome: {funcionarioRemovido.Nome}, Idade: {funcionarioRemovido.Idade}, Telefone: {funcionarioRemovido.Telefone}, Salário: {funcionarioRemovido.Salario}");
+            }
+
+            else{
+            No atual = primeiro;
+            No anterior = null;
+
+            while (atual.Proximo != null)
+            {
+                anterior = atual;
+                atual = atual.Proximo;
+            }
+
+            Funcionario funcionarioRemovido = atual.Dado;
+            anterior.Proximo = null;
+            ultimo = anterior;
+            Console.WriteLine($"Funcionário removido do fim da lista - Nome: {funcionarioRemovido.Nome}, Idade: {funcionarioRemovido.Idade}, Telefone: {funcionarioRemovido.Telefone}, Salário: {funcionarioRemovido.Salario}");
+                }
+            }            
+         }
+            //exibir
+            else if( escolhido == 6){
+            Exibir();
+            }
     
+
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,29 +1,54 @@
-﻿namespace AulaListaSE{
+﻿using System;
+namespace AulaListaSE{
 
     class Program{
         static void Main(string[] args){
 
-            Lista lista = new();
-            lista.inserirInicio(22);
-            lista.inserirInicio(12);
-            lista.inserirInicio(7);
-            lista.removerInicio();
-            lista.inserirFim(10);
-            lista.removerFim();
-            lista.removerInicio();
-            //7 -> 12 -> 22 -> NULL
-            lista.percurso();
+            //--CONSULTA
 
-            No noAtual = null;
-            No noAnterior = null;
-            Boolean encontrou = lista.consulta(12, ref noAtual, ref noAnterior);
-            if(encontrou){
-                Console.WriteLine("Encontrou o no de valor: " + noAtual.valor);
-            }
-            else{
-                Console.WriteLine("Não encontrou o no procurado!");
-            }
+            //No noAtual = null;
+            //No noAnterior = null;
+            //Boolean encontrou = lista.consulta(12, ref noAtual, ref noAnterior);
+            //if(encontrou){
+                //Console.WriteLine("Encontrou o no de valor: " + noAtual.valor);
+            //}
+            //else{
+             //   Console.WriteLine("Não encontrou o no procurado!");
+            //}
+
+            Lista lista = new Lista();
+
+            //Exibindo os funcionários da lista
+
+
+            while (true)
+            {
+                Console.Write("Escolha:");
+                Console.Write("\nInserir -> 1");
+                Console.Write("\nInserir no Fim -> 2");
+                Console.Write("\nBuscar -> 3");
+                Console.Write("\nRemover -> 4");
+                Console.Write("\nRemover Fim -> 5");
+                Console.Write("\nPercuso -> 6\n");
+                Console.Write("SAIR -> 7\n\n");
+                lista.escolhido = Convert.ToInt32(Console.ReadLine());
+
+                if (lista.escolhido >= 1 && lista.escolhido <= 6)
+                {
+                    lista.Escolha();
+                }
+                else if (lista.escolhido == 7)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Opção inválida. Por favor, tente novamente.");
+                }
+
         }
     }
 }
+}
+
 
